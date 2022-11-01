5e72d8f724bad960db0bb8ea28ec222294d0ec87
keyaedisa <46032999+keyaedisa@users.noreply.github.com>
2022-11-01 16:15:01 -0400

    combed over entire 002SE profile, newKaiso lives on in 002SE full circle moment

--- 002SE/profiledef.sh
+++ 002SE/profiledef.sh
@@ -1,30 +1,24 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="002-Server-Edition-Alpha"
-iso_label="002SE-Alpha"
-iso_publisher="Keyaedisa"
-iso_application="002SE Live/Rescue CD"
-iso_version="0.0.1a"
-install_dir="arch"
+iso_name="newKaisoEXP"
+iso_label="newKaisoEXP_0.7e"
+iso_publisher="keyaedisa <https://twitter.com/keyaedisa>"
+iso_application="newKaiEXP installer"
+iso_version="0.7e"
+install_dir="newKaiEXP"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-          'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
-   ["/etc/shadow"]="0:0:600"
-   ["/root"]="0:0:700"
-   ["/etc/polkit-1/rules.d"]="0:0:750"
-   ["/etc/sudoers.d"]="0:0:750"
-   ["/root/.automated_script.sh"]="0:0:755"
-#  ["/usr/local/bin/vcheck"]="0:0:755"
-#  ["/usr/local/bin/dmcheck"]="0:0:755"
-   ["/usr/local/bin/xerolinux-before"]="0:0:755"
-#  ["/usr/local/bin/skel"]="0:0:755"
-#  ["/usr/local/bin/all-cores"]="0:0:755"
-   ["/usr/local/bin/xerolinux-final"]="0:0:755"
-#  ["/usr/local/bin/services"]="0:0:755"
-   )
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
