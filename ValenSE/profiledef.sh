e756f5fc2c39b20a7b83f1c164a5ca84f3007fde
keyaedisa <46032999+keyaedisa@users.noreply.github.com>
2023-01-30 17:22:49 -0500

    beginning of new iso for my server

--- ValenSE/profiledef.sh
+++ ValenSE/profiledef.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="ValenSE-Alpha"
-iso_label="ValenSE"
-iso_publisher="keyaedisa <https://github.com/keyaedisa/ValenSE>"
-iso_application="Valen Server Edition Installer"
-iso_version="0.1g"
-install_dir="ValenSE"
+iso_name="ValenSE"
+iso_label="ValenSE-beta"
+iso_publisher="Keyaedisa"
+iso_application="ValenSE Live/Rescue CD"
+iso_version="0.0.1a"
+install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+          'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/etc/gshadow"]="0:0:400"
-  ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
-)
+   ["/etc/shadow"]="0:0:600"
+   ["/root"]="0:0:700"
+   ["/etc/polkit-1/rules.d"]="0:0:750"
+   ["/etc/sudoers.d"]="0:0:750"
+   ["/root/.automated_script.sh"]="0:0:755"
+#  ["/usr/local/bin/vcheck"]="0:0:755"
+#  ["/usr/local/bin/dmcheck"]="0:0:755"
+   ["/usr/local/bin/xerolinux-before"]="0:0:755"
+#  ["/usr/local/bin/skel"]="0:0:755"
+#  ["/usr/local/bin/all-cores"]="0:0:755"
+   ["/usr/local/bin/xerolinux-final"]="0:0:755"
+#  ["/usr/local/bin/services"]="0:0:755"
+   )
